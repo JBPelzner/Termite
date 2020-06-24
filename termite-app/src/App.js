543bed9762c9ea89a837d4d6a9167e36b287efe0
jenpatterson97 <jenpatterson@berkeley.edu>
2020-06-23 18:25:30 -0700

    initial commit

---
+++ termite-app/src/App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      
+    <h1>Termite</h1>
+   
+    
+
+    <h2>About Our Classifier</h2>
+
+
+
+
+    <h2>User Preferences</h2>
+
+
+
+
+    <h2>User reports</h2>
+    
+
+
+
+
+    </div>
+  );
+}
+
+export default App;
